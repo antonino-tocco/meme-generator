2842f0079172764999d46c775fecdc192af8a033
Antonino Tocco <entonytocco@gmail.com>
2023-03-13 06:29:20 +0100

    fix for font

--- src/MemeEngine/meme_engine.py
+++ src/MemeEngine/meme_engine.py
@@ -15,12 +15,13 @@ class MemeEngine:
     def make_meme(self, img_path, text, author, width=500) -> str:
         """ Create a meme given an image and a quote """
         try:
+            font_path = os.path.join(os.path.dirname(__file__), '..', 'resources', 'roboto-regular.ttf')
             output_path = os.path.join(self.output_dir, 'meme.jpg')
             image = Image.open(img_path)
             height = int(width * image.height / image.width)
             image = image.resize((width, height))
             draw = ImageDraw.Draw(image)
-            font = ImageFont.truetype('arial.ttf', 30)
+            font = ImageFont.truetype(font_path, 30)
             draw.text((10, (height - 100)), text, fill='white', font=font)
             draw.text((10, (height - 50)), author, fill='white', font=font)
             image.save(output_path)
